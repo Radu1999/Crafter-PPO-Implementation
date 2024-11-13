972c1cff0e4ee5fd292b9685ddf7d82ccb95806d
rchivereanu <rchivereanu@adobe.com>
2024-11-13 14:25:49 +0200

    update

--- play.py
+++ play.py
@@ -8,20 +8,22 @@ import torch
 class opt:
     logdir = "crafter"
     history_length = 4
-    device = "cpu"
     save_video = True
 
 
-env = Env("train", opt)
-policy = NN_Policy(env.observation_space, env.action_space)
-agent = PPOAgent(policy=policy, logger=wandb, device=opt.device)
+if __name__ == "__main__":
 
-obs = env.reset()
-done = False
+    opt.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    env = Env("train", opt)
+    policy = NN_Policy(env.observation_space, env.action_space).to(opt.device)
+    agent = PPOAgent(policy=policy, logger=wandb, device=opt.device).to(opt.device)
 
-agent.policy.load_state_dict(torch.load('best_model.pth', weights_only=True))
-while not done:
-    with torch.no_grad():
-        action, value = agent.act(obs)
-    print(action)
-    obs, reward, done, info = env.step(action)
+    obs = env.reset()
+    done = False
+
+    agent.policy.load_state_dict(torch.load('best_model.pth', weights_only=True))
+    while not done:
+        with torch.no_grad():
+            action, value = agent.act(obs)
+        print(action)
+        obs, reward, done, info = env.step(action)
